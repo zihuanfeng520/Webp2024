3e283cf8b5989594c3807d115c4b2ddbe927f77e
王英瑋 <zihuanfeng520@gmail.com>
2024-05-21 22:08:20 +0800

    Delete HW#3/react_datagrid/src directory

--- HW#3/react_datagrid/src/cgu_hello.js
+++
@@ -1,7 +0,0 @@
-import './App.css';
-const styleArgument = { fontSize: '100px', color: 'red' };
-const HelloCGU=()=>{ 
-  return <h1 style = { styleArgument } > 景點觀光展覽資訊 </h1>; 
-}
-
-export default HelloCGU;
